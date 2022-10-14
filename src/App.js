a8c5f8a711a2072944ac6d224212c44ce5cbe3f0
Aabha Aggarwal <aabha.aggarwal@capgemini.com>
2022-10-14 09:47:26 +0530

    removed commented code from app.js

--- src/App.js
+++ src/App.js
@@ -37,39 +37,21 @@ function App() {
         <Route path="/flatbooking/tenant/:id" element={<ViewAllByTenant />}></Route>
         <Route path="/flatbooking/approval/all" element={<ApprovalFlatBooking />}></Route>
         <Route path="/flatbooking/approval/:id" element={<ViewFlatBookingApproval />}></Route>
-        {/* <Route path='/' element={<Home />}></Route> */}
-
         <Route path="/landlord/all" element={<ViewAllLandlord />}></Route>
-
-        {/* <Route path="/landlord/update/:id" element={<UpdateLandlord />}></Route> */}
-
         <Route path="/landlord/details/:id" element={<ProfileLandlord />}></Route>
-
         <Route path="/landlord/delete/:id" element={<DeleteLandlord />}></Route>
-
-        {/* <Route path='/landlord/add' element={<AddLandlord />}></Route> */}
         <Route path="/flat/details/:id" element={<ViewFlatById />} />
-
-        <Route path="/flat/all" element={<ViewAllFlats/>} />
-
+        <Route path="/flat/all" element={<ViewAllFlats />} />
         <Route path="/" element={<Homepage />} />
-        <Route path="/navbar" element={<Navbar/>} />
-        <Route path="/navbar/tenant" element={<NavbarTenant/>} />
-        <Route path="/navbar/landlord" element={<NavbarLandlord/>} />
-        {/* <Route path="/" element={<Home />}></Route> */}
-        <Route path='/signup' element={<AddUser/>}></Route>
-
-{/* <Route path="/tenant/update/:id" element={<UpdateTenant />}></Route> */}
-
-<Route path="/tenant/all" element={<ViewAllTenant />}></Route>
-
-{/* <Route path="/tenant/profile" element={<TenantProfile />}></Route> */}
-
-<Route path="/tenant/details/:id" element={<TenantProfile/>}></Route>
-
-<Route path="/tenant/delete/:id" element={<DeleteTenant />}></Route>
-<Route path="/login" element={<Login />}></Route>
-<Route path="/logout" element={<Logout />}></Route>
+        <Route path="/navbar" element={<Navbar />} />
+        <Route path="/navbar/tenant" element={<NavbarTenant />} />
+        <Route path="/navbar/landlord" element={<NavbarLandlord />} />
+        <Route path='/signup' element={<AddUser />}></Route>
+        <Route path="/tenant/all" element={<ViewAllTenant />}></Route>
+        <Route path="/tenant/details/:id" element={<TenantProfile />}></Route>
+        <Route path="/tenant/delete/:id" element={<DeleteTenant />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/logout" element={<Logout />}></Route>
       </Routes>
     </BrowserRouter>
   );
