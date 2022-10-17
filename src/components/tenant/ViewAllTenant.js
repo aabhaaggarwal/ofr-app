c75c509f3d7383eb59d9b472820dabafe83b97b8
Anshika Soni <ANSHIKA.SONI@CAPGEMINI.com>
2022-10-17 12:44:37 +0530

    view all change commit

--- src/components/tenant/ViewAllTenant.js
+++ src/components/tenant/ViewAllTenant.js
@@ -11,52 +11,51 @@ function ViewAllTenant() {
         axios.get("http://localhost:8080/tenant/all").then(resp => setTenants (resp.data));
           },[]);
     
-  return (
-    <div class="row">
-      <NavbarAdmin/>
-      <div class="col-9">
-    <div className="container shadow-lg mt-5">
-      {
-        tenants.length > 0 &&
-
-        <table className="table">
-            <thead className="thead-dark">
-            <tr>
-                <th>UserId</th>
-                <th>UserName</th>
-                <th>Password</th>
-                <th>Email</th>
-                <th>Role</th>
-                <th>FirstName</th>
-                <th>LastName</th>
-                <th>Age</th>
-                <th>Gender</th>
-                <th>Mobile</th> 
-                <th></th>    
-            </tr>
-            </thead>
-            <tbody>
+          return (
+            <div class="row">
+          <NavbarAdmin/>
+          <div class="col-9">
+            <div className="container shadow-lg mt-5">
                 {
-                    tenants.map(t => <tr key={t.userId}>
-                        <td>{t.userId}</td>
-                        <td>{t.username}</td>
-                        <td>{t.password}</td>
-                        <td>{t.email}</td>
-                        <td>{t.role}</td>
-                        <td>{t.firstName}</td>
-                        <td>{t.lastName}</td>
-                        <td>{t.age}</td>
-                        <td>{t.gender}</td>
-                        <td>{t.mobile}</td>
-                        <td><Link to={`/tenant/delete/${t.userId}`}><i class="fa fa-trash" aria-hidden="true"></i></Link></td>
-                       </tr>)
+                    tenants.length > 0 &&
+    
+                    <table className="table">
+                        <thead className="thead-dark">
+                            <tr>
+                                <th>UserId</th>
+                                <th>UserName</th>
+                                <th>Password</th>
+                                <th>FirstName</th>
+                                <th>LastName</th>
+                                <th>Email</th>
+                                <th>Age</th>
+                                <th>Gender</th>
+                                <th>Mobile</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                landlords.map(l => <tr key={l.userId}>
+                                    <td>{t.userId}</td>
+                                    <td>{t.username}</td>
+                                    <td>{t.password} </td>
+                                    <td>{t.firstName}</td>
+                                    <td>{t.lastName}</td>
+                                    <td> {t.email}</td>
+                                    <td> {t.age}</td>
+                                    <td> {t.gender}</td>
+                                    <td> {t.mobile}</td>
+                                    <td><Link to={`/tenant/delete/${t.userId}`}><i class="fa fa-trash" aria-hidden="true"></i></Link></td>
+                                </tr>)
+                            }
+                        </tbody>
+                    </table>
                 }
-            </tbody>
-        </table>
-      }
-    </div>
-    </div>
-    </div>
-  )
+    
+            </div>
+            </div>
+            </div>
+        )
 }
 export default ViewAllTenant;
